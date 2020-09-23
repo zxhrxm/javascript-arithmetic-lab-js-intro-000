15fe500e98f243a6cf2f24fc966623fe4216492a
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-23 15:55:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function decrement(n) {
 }
 
 function makeInt(string) {
-  return parseInt('string', 10)
+  return parseInt(string, 10)
 }
 
 function preserveDecimal(string) {
-  return parseFloat('string')
+  return parseFloat(string)
 }
